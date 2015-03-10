df8b0c93e5d7c2c280f53a1faaaf3184b0e12508
plsg <patrick@patricklesage.ca>
2015-03-10 16:24:57 -0400

    create About-us page and Our-motto sidebar

--- sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
@@ -164,11 +164,11 @@
   </div>
 </div>
 
-<?php if (!empty($page['footer'])): ?>
+<?php //if (!empty($page['footer'])): ?>
   <footer class="footer container">
       <?php print render($page['footer']); ?>
   </footer>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
   <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-<?php endif; ?>
+<?php //endif; ?>
