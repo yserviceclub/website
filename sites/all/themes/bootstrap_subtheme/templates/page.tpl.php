b4e96943d1231586396e50aeb293ea5993a9da44
plsg <patrick@patricklesage.ca>
2015-03-13 10:33:23 -0400

    repair login issue
    install bootstrap_login_modal which somehow fixes the user/login page
    access denied issue.
    
    Separate css stylesheets for easier editing.

--- sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
@@ -98,14 +98,14 @@
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
         <nav role="navigation">
+          <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
           <?php endif; ?>
           <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
           <?php endif; ?>
         </nav>
       </div>
@@ -168,7 +168,6 @@
   <footer class="footer container">
       <?php print render($page['footer']); ?>
   </footer>
-
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
   <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 <?php //endif; ?>
